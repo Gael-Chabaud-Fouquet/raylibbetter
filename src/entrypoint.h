f766dc1ced6768bee69ccf6524fac7cc7125d252
Gael-Chabaud-Fouquet <2430057@bart.ca>
2025-04-15 13:17:32 -0400

    first commit with basic modification that wont be needed to be done in the future

--- src/entrypoint.h
+++ src/entrypoint.h
@@ -1,11 +1,14 @@
 #pragma once
+#include <raylib.h>
 
-#ifdef __cplusplus
-extern "C"{
-#endif
 
-void raylib_start(void);
 
-#ifdef __cplusplus
-}
-#endif
+// #ifdef __cplusplus
+// extern "C"{
+// #endif
+
+// void raylib_start(void);
+
+// #ifdef __cplusplus
+// }
+// #endif
